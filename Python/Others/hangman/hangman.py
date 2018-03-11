deb8ad5d43f50ad02462f7e055d9848162716c96
artur855 <artur855@hotmail.com>
2018-03-10 23:56:06 -0300

    Improve hangman

--- Python/Others/hangman/hangman.py
+++ Python/Others/hangman/hangman.py
@@ -1,35 +1,42 @@
 import random
-words = ['house', 'rat', 'table', 'dog',
-         'door', 'school', 'computer', 'internet']
+import os
+
+file = open(os.path.join(os.getcwd()+'\\hangman', 'dictionary.txt'), 'r')
+words = file.read().splitlines()
 answer = random.choice(words)
 track = ['_' for i in answer]
-lifes = 5
+guessed = []
+life = 5
 print('Welcome to the hangman game')
-while(lifes > 0):
-    print('\n----------------------------------------------------------')
+
+print('\n----------------------------------------------------------')
+while life > 0:
 
     print('Progress: ' + ' '.join(track))
-    print('Remaining chances: ' + str(lifes))
+    print('Remaining chances: ' + str(life))
+    if len(guessed) > 0:
+        print('Guessed letters: ['+', '.join(guessed)+']')
     print('Guess a letter:')
     chute = input()
     print('----------------------------------------------------------\n')
-    indexs = []
-    if chute in track:
+    index = []
+    if chute in guessed:
         print('You already guessed that letter')
     elif len(chute) != 1:
         print('Your guess can only one letter and no blank spaces')
     else:
-        indexs = [i for i, ltr in enumerate(answer) if ltr == chute]
-        if len(indexs) > 0:
-            for i in indexs:
+        index = [i for i, ltr in enumerate(answer) if ltr == chute]
+        if len(index) > 0:
+            for i in index:
                 track[i] = chute
         else:
             print("The word doesn't contain the guessed letter")
-            lifes -= 1
+            life -= 1
+    guessed.append(chute)
     if list(answer) == track:
         print('Congratulations!!!')
         print('You guessed the word "{}" correctly'.format(answer))
         break
-    if lifes == 0:
+    if life == 0:
         print('\nYou lost!! :(')
         print('The correct word is: "' + answer + '"')
